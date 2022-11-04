aeed2433c1da588f1325d4ae951355cd4e52e8f5
Mateus Lucena <mateuslucena2014@gmail.com>
2022-11-04 11:30:31 -0300

    Primeiro de tudo

---
+++ src/Time.java
@@ -0,0 +1,20 @@
+public class Time {
+    public String nomeTime;
+    public String siglaTime;
+
+    public String getNomeTime() {
+        return nomeTime;
+    }
+
+    public String getSiglaTime() {
+        return siglaTime;
+    }
+
+    public void setNomeTime(String nomeTime) {
+        this.nomeTime = nomeTime;
+    }
+
+    public void setSiglaTime(String siglaTime) {
+        this.siglaTime = siglaTime;
+    }
+}
