cb1c919d70f0edacaffb76a3d94d64a105347a19
BrunoEduGomes <114311965+BrunoEduGomes@users.noreply.github.com>
2022-11-10 16:07:06 -0300

    Update TabelaCopa.java

--- src/TabelaCopa.java
+++ src/TabelaCopa.java
@@ -3,12 +3,14 @@ public class TabelaCopa {
     private Jogo jogo;
     private Time campeão;
     private int rodadas;
+    private VencedoresCopa vencedores;
 
     public TabelaCopa(){
         times = new Times();
         jogo = new Jogo();
         campeão = null;
         rodadas = -1;
+        vencedores = new VencedoresCopa();
     }
 
     // Métodos que pegam os atributos do objeto TabelaCopa.
@@ -37,7 +39,7 @@ public class TabelaCopa {
 
     // Método que gera rodadas.
     public int setRodadas(){
-        int n = 0;
+        int n = -1;
         if (times.getArray().size() > 16) {
             n = -1;
         }
@@ -59,20 +61,124 @@ public class TabelaCopa {
     //
 
 
-    // APRIMORAR AMANHÃ: QUANDO RODADA = 4, VENCEDOR DO JOGO ADICIONADO NA LISTA DOS VENCEDORES DAS QUARTAS.(EXEMPLO, VERIFICAR LÓGICA)
+    // APRIMORAR: Fazer os gols serem randômicos. (Fora daqui: criar a classe campeonato.)
     public void organizaCampeonato(){
-        int b = times.getArray().size() - 1;
-        if ((times.getArray().size())%2 == 0){
+        rodadas = setRodadas();
+        if (rodadas == 4){
+            int b = times.getArray().size() - 1;
+            System.out.println(" \nOITAVAS DE FINAL!\n ");
             while (b > 0){
-               System.out.println();
                jogo.setTime(times.getArray().get(b), times.getArray().get(b-1));
-               jogo.setGols(b, b);
+               jogo.setGols(b, b+1);
+               vencedores.setQuartas(jogo.partida());
                System.out.println(jogo.organizaJogo());
                System.out.println();
+               if (b == 1){
+                  rodadas = 3;
+               }
                b = b -2;
             }
         }
-        else {System.out.println("\nSó da pra criar copa com 16, 8, 4 ou 2 times.\n");}
+        if (rodadas == 3){
+            if (times.getArray().size() == 8){
+                int b = times.getArray().size() - 1;
+                System.out.println(" \nQUARTAS DE FINAL\n ");
+                while (b > 0){
+                   jogo.setTime(times.getArray().get(b), times.getArray().get(b-1));
+                   jogo.setGols(b, b+1);
+                   vencedores.setSemi(jogo.partida());
+                   System.out.println(jogo.organizaJogo());
+                   System.out.println();
+                   if (b == 1){
+                       rodadas = 2;
+                   }
+                   b = b -2;
+                }
+            }
+            else {
+                int b = vencedores.getQuartas().size() - 1;
+                System.out.println(" \nQUARTAS DE FINAL!\n ");
+                while (b > 0){
+                    jogo.setTime(vencedores.getQuartas().get(b), vencedores.getQuartas().get(b-1));
+                    jogo.setGols(b, b+1);
+                    vencedores.setSemi(jogo.partida());
+                    System.out.println(jogo.organizaJogo());
+                    System.out.println();
+                    if (b == 1){
+                       rodadas = 2;
+                    }
+                    b = b -2;
+                }
+            }
+        }
+        if (rodadas == 2){
+            if (times.getArray().size() == 4){
+                int b = times.getArray().size() - 1;
+                System.out.println(" \nSEMIFINAL\n ");
+                while (b > 0){
+                   jogo.setTime(times.getArray().get(b), times.getArray().get(b-1));
+                   jogo.setGols(b, b+1);
+                   vencedores.setFinal(jogo.partida());
+                   System.out.println(jogo.organizaJogo());
+                   System.out.println();
+                   if (b == 1){
+                       rodadas = 1;
+                   }
+                   b = b -2;
+                }
+            }
+            else{
+                int b = vencedores.getSemi().size() - 1;
+                System.out.println(" \nSEMIFINAL!\n ");
+                while (b > 0){
+                    jogo.setTime(vencedores.getSemi().get(b), vencedores.getSemi().get(b-1));
+                    jogo.setGols(b, b+1);
+                    vencedores.setFinal(jogo.partida());
+                    System.out.println(jogo.organizaJogo());
+                    System.out.println();
+                    if (b == 1){
+                       rodadas = 1;
+                    }
+                    b = b -2;
+                }
+            }
+        }
+        if (rodadas == 1){
+            if (times.getArray().size() == 2){
+                int b = times.getArray().size() - 1;
+                System.out.println(" \nFINAL!\n ");
+                while (b > 0){
+                   jogo.setTime(times.getArray().get(b), times.getArray().get(b-1));
+                   jogo.setGols(b, b+1);
+                   campeão = jogo.partida();
+                   System.out.println(jogo.organizaJogo());
+                   System.out.println();
+                   if (b == 1){
+                       rodadas = 99;
+                   }
+                   b = b -2;
+                }
+            }
+            else{
+                int b = vencedores.getFinal().size() - 1;
+                System.out.println(" \nFINAL!\n ");
+                while (b > 0){
+                    jogo.setTime(vencedores.getFinal().get(b), vencedores.getFinal().get(b-1));
+                    jogo.setGols(b, b+1);
+                    campeão = jogo.partida();
+                    System.out.println(jogo.organizaJogo());
+                    System.out.println();
+                    if (b == 1){
+                       rodadas = 99;
+                    }
+                    b = b -2;
+                }
+            }
+        }
+        if (rodadas == 99){
+            System.out.println("CAMPEÃO:" + campeão.getNome() + "\n");
+        }
+        if (rodadas == -1) {System.out.println("\nSó da pra criar copa com 16, 8, 4 ou 2 times. Veja bem quantos times você inseriu!!\n");}
     }
 
 }
