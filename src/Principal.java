91b61948ab90b9e8a249406773d4b0adf26ca3e8
BrunoEduGomes <114311965+BrunoEduGomes@users.noreply.github.com>
2022-11-10 16:10:24 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -7,7 +7,22 @@ public class Principal {
         copa.setArray(new Time("URUGUAI", "URA"));
         copa.setArray(new Time("ALEMANHA", "GER"));
         copa.setArray(new Time("FRANÇA", "FRA"));
-        //
+        
+        copa.setArray(new Time("iTALIA", "ITA"));
+        copa.setArray(new Time("ARGENTINA", "ARG"));
+        copa.setArray(new Time("EST.UNIDOS", "EUA"));
+        copa.setArray(new Time("RUSSIA", "RUS"));
+        
+        copa.setArray(new Time("QATAR", "QAT"));
+        copa.setArray(new Time("JAPAO", "JAP"));
+        copa.setArray(new Time("COREIA SUL", "COR"));
+        copa.setArray(new Time("CHINA", "CHI"));
+         
+        copa.setArray(new Time("BRASIL", "BRA"));
+        copa.setArray(new Time("URUGUAI", "URA"));
+        copa.setArray(new Time("ALEMANHA", "GER"));
+        copa.setArray(new Time("FRANÇA", "FRA"));
+        
 
         copa.organizaCampeonato();
 
