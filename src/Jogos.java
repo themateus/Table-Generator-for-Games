107c5a06758a4aaeec0bf7c4b1098a232f194eee
BrunoEduGomes <114311965+BrunoEduGomes@users.noreply.github.com>
2022-11-14 17:47:43 -0300

    Update Jogos.java

--- src/Jogos.java
+++ src/Jogos.java
@@ -2,22 +2,18 @@ import java.util.ArrayList;
 
 public class Jogos {
     private ArrayList<Jogo> jogos;
-    private ArrayList<Jogo> jogosPassados;
 
     //Construtor.
     public Jogos(){
         jogos = new ArrayList<>();
-        jogosPassados = new ArrayList<>();
     }
+    //
 
     // Método que retorna a lista de jogos.
     public ArrayList<Jogo> getJogos(){
         return jogos;
     }
-
-    public ArrayList<Jogo> getJogosPassados(){
-        return jogosPassados;
-    }
+    //
 
     // Método que adiciona um jogo na lista de jogos.
     public void setArrayJogo(Jogo jogo){
@@ -25,11 +21,6 @@ public class Jogos {
         if ((jogo.getTime1() == null) || (jogo.getTime2() == null)){}
         else {jogos.add(jogo);}
     }
-
-    public void setArrayJogoPassado(Jogo jogo){
-        if (jogo == null){}
-        if ((jogo.getTime1() == null) || (jogo.getTime2() == null)){}
-        else {jogosPassados.add(jogo);}
-    }
+    //
 
 }
